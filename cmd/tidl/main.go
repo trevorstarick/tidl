945cb102f9092f95ad065824b8c53951a5d885ea
Trevor Starick <trevor.starick@gmail.com>
2018-11-01 05:47:37 -0400

    seperates tidl into a library (tidl.go) and an executable (cmd/tidl/main.go)

---
+++ cmd/tidl/main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/trevorstarick/tidl"
+)
+
+var username, password string
+
+var onlyAlbums = flag.Bool("albums", false, "only download albums")
+var onlyEPs = flag.Bool("eps", false, "only download eps and singles")
+
+func main() {
+	var err error
+
+	flag.Parse()
+
+	// TODO(ts): handle output better
+	// TODO(ts): handle no input
+	if len(flag.Args()) == 0 {
+		return
+	}
+
+	var ids []string
+
+	if _, err = os.Stat(flag.Args()[0]); !os.IsNotExist(err) {
+		f, err := os.Open(flag.Args()[0])
+		if err != nil {
+			panic(err)
+		}
+
+		buffer := bufio.NewScanner(f)
+		for buffer.Scan() {
+			ids = append(ids, buffer.Text())
+		}
+	} else {
+		ids = flag.Args()
+	}
+
+	t, err := tidl.New(username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, id := range ids {
+		var albums []tidl.Album
+
+		if id[0] == 'h' {
+			id = strings.Split(id, "album/")[1]
+		}
+
+		// TODO(ts): support fetching of EP/Singles as well as flags to disable
+		// TODO(ts): support fetching of artist info
+		artist, err := t.GetArtist(id)
+		if err != nil {
+			panic(err)
+		}
+
+		if artist.ID.String() != "" {
+			fmt.Printf("Downloading %v (%v)...\n", artist.Name, artist.ID)
+
+			if *onlyAlbums == true {
+				fmt.Println("Only fetching Albums")
+				lbums, err := t.GetArtistAlbums(id, 0)
+				if err != nil {
+					panic(err)
+				}
+
+				albums = append(albums, lbums...)
+			} else if *onlyEPs {
+				fmt.Println("Only fetching EPs & Singles")
+				lbums, err := t.GetArtistEP(id, 0)
+				if err != nil {
+					panic(err)
+				}
+
+				albums = append(albums, lbums...)
+			} else {
+				fmt.Println("Fetching Albums, EPs & Singles")
+				lbums, err := t.GetArtistAlbums(id, 0)
+				if err != nil {
+					panic(err)
+				}
+
+				albums = append(albums, lbums...)
+
+				lbums, err = t.GetArtistEP(id, 0)
+				if err != nil {
+					panic(err)
+				}
+
+				albums = append(albums, lbums...)
+			}
+		} else {
+			album, err := t.GetAlbum(id)
+			if err != nil {
+				panic(err)
+			}
+
+			albums = []tidl.Album{album}
+		}
+
+		albumMap := make(map[string]tidl.Album)
+		for _, album := range albums {
+			if _, ok := albumMap[album.Title]; !ok {
+				albumMap[album.Title] = album
+			} else {
+				// TODO(ts): impove dedupe if statement
+
+				if album.AudioQuality == "LOSSLESS" && albumMap[album.Title].AudioQuality != "LOSSLESS" {
+					// if higher quality
+					albumMap[album.Title] = album
+				} else if album.Explicit && !albumMap[album.Title].Explicit {
+					// if explicit
+					albumMap[album.Title] = album
+				} else if album.Popularity > albumMap[album.Title].Popularity {
+					// if more popular
+					albumMap[album.Title] = album
+				}
+			}
+		}
+
+		albums = make([]tidl.Album, 0, len(albumMap))
+		for _, album := range albumMap {
+			albums = append(albums, album)
+		}
+
+		for _, album := range albums {
+			fmt.Printf("[%v] %v\n", album.ID.String(), album.Title)
+			t.DownloadAlbum(album)
+			// 	tracks, err := t.GetAlbumTracks(album.ID.String())
+			// 	if err != nil {
+			// 		panic(err)
+			// 	}
+
+			// 	processQueue := make(chan Track, len(tracks))
+			// 	for _, track := range tracks {
+			// 		processQueue <- track
+			// 	}
+			// 	close(processQueue)
+
+			// 	wg := sync.WaitGroup{}
+
+			// 	for i := 0; i < 8; i++ {
+			// 		wg.Add(1)
+			// 		go func() {
+			// 			for track := range processQueue {
+			// 				t.DownloadTrack(track)
+			// 				fmt.Printf("\t%v\n", track.Title)
+			// 			}
+			// 			wg.Done()
+			// 		}()
+			// 	}
+
+			// 	wg.Wait()
+		}
+	}
+}
--- tidl.go
+++ tidl.go
@@ -1,12 +1,10 @@
-package main
+package tidl
 
 import (
-	"bufio"
 	"bytes"
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -18,7 +16,6 @@ import (
 	"strings"
 
 	// TODO(ts): look at replacing bitio
-
 	"github.com/icza/bitio"
 	"github.com/mewkiz/flac"
 	"github.com/mewkiz/flac/meta"
@@ -33,15 +30,10 @@ const (
 	AQ_HI_RES
 )
 
-var username, password string
-
 var cookieJar, _ = cookiejar.New(nil)
 var c = &http.Client{
 	Jar: cookieJar,
 }
-
-var onlyAlbums = flag.Bool("albums", false, "only download albums")
-var onlyEPs = flag.Bool("eps", false, "only download eps and singles")
 
 type TidalError struct {
 	Status      int
@@ -484,151 +476,4 @@ func enc(src string, tr Track) error {
 	f.Close()
 	stream.Close()
 	return err
-}
-
-func main() {
-	var err error
-
-	flag.Parse()
-
-	// TODO(ts): handle output better
-	// TODO(ts): handle no input
-	if len(flag.Args()) == 0 {
-		return
-	}
-
-	var ids []string
-
-	if _, err = os.Stat(flag.Args()[0]); !os.IsNotExist(err) {
-		f, err := os.Open(flag.Args()[0])
-		if err != nil {
-			panic(err)
-		}
-
-		buffer := bufio.NewScanner(f)
-		for buffer.Scan() {
-			ids = append(ids, buffer.Text())
-		}
-	} else {
-		ids = flag.Args()
-	}
-
-	t, err := New(username, password)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, id := range ids {
-		var albums []Album
-
-		if id[0] == 'h' {
-			id = strings.Split(id, "album/")[1]
-		}
-
-		// TODO(ts): support fetching of EP/Singles as well as flags to disable
-		// TODO(ts): support fetching of artist info
-		artist, err := t.GetArtist(id)
-		if err != nil {
-			panic(err)
-		}
-
-		if artist.ID.String() != "" {
-			fmt.Printf("Downloading %v (%v)...\n", artist.Name, artist.ID)
-
-			if *onlyAlbums == true {
-				fmt.Println("Only fetching Albums")
-				lbums, err := t.GetArtistAlbums(id, 0)
-				if err != nil {
-					panic(err)
-				}
-
-				albums = append(albums, lbums...)
-			} else if *onlyEPs {
-				fmt.Println("Only fetching EPs & Singles")
-				lbums, err := t.GetArtistEP(id, 0)
-				if err != nil {
-					panic(err)
-				}
-
-				albums = append(albums, lbums...)
-			} else {
-				fmt.Println("Fetching Albums, EPs & Singles")
-				lbums, err := t.GetArtistAlbums(id, 0)
-				if err != nil {
-					panic(err)
-				}
-
-				albums = append(albums, lbums...)
-
-				lbums, err = t.GetArtistEP(id, 0)
-				if err != nil {
-					panic(err)
-				}
-
-				albums = append(albums, lbums...)
-			}
-		} else {
-			album, err := t.GetAlbum(id)
-			if err != nil {
-				panic(err)
-			}
-
-			albums = []Album{album}
-		}
-
-		albumMap := make(map[string]Album)
-		for _, album := range albums {
-			if _, ok := albumMap[album.Title]; !ok {
-				albumMap[album.Title] = album
-			} else {
-				// TODO(ts): impove dedupe if statement
-
-				if album.AudioQuality == "LOSSLESS" && albumMap[album.Title].AudioQuality != "LOSSLESS" {
-					// if higher quality
-					albumMap[album.Title] = album
-				} else if album.Explicit && !albumMap[album.Title].Explicit {
-					// if explicit
-					albumMap[album.Title] = album
-				} else if album.Popularity > albumMap[album.Title].Popularity {
-					// if more popular
-					albumMap[album.Title] = album
-				}
-			}
-		}
-
-		albums = make([]Album, 0, len(albumMap))
-		for _, album := range albumMap {
-			albums = append(albums, album)
-		}
-
-		for _, album := range albums {
-			fmt.Printf("[%v] %v\n", album.ID.String(), album.Title)
-			t.DownloadAlbum(album)
-			// 	tracks, err := t.GetAlbumTracks(album.ID.String())
-			// 	if err != nil {
-			// 		panic(err)
-			// 	}
-
-			// 	processQueue := make(chan Track, len(tracks))
-			// 	for _, track := range tracks {
-			// 		processQueue <- track
-			// 	}
-			// 	close(processQueue)
-
-			// 	wg := sync.WaitGroup{}
-
-			// 	for i := 0; i < 8; i++ {
-			// 		wg.Add(1)
-			// 		go func() {
-			// 			for track := range processQueue {
-			// 				t.DownloadTrack(track)
-			// 				fmt.Printf("\t%v\n", track.Title)
-			// 			}
-			// 			wg.Done()
-			// 		}()
-			// 	}
-
-			// 	wg.Wait()
-		}
-	}
 }
